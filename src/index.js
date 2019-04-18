4be2a129902de8ea4d6a2aec3604b2610b10c011
Nam Le <nam.leduc02@gmail.com>
2019-04-18 13:57:49 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>My app</div>,
+  document.getElementById('app')
+)
+
+module.hot.accept()
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require('webpack');
+
+module.exports = {
+    module: {
+    rules: [
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        },
+        {
+            test: /\.html$/,
+            use: [
+                {
+                loader: "html-loader"
+                }
+            ]
+        }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "./index.html"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    }
+};
