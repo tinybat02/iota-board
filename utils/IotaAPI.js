0c1a72c96f2f556a8729a2acc940e9e385e3c11a
Nam Le <nam.leduc02@gmail.com>
2019-05-31 15:30:29 +0200

    Update util func

--- utils/IotaAPI.js
+++ utils/IotaAPI.js
@@ -3,36 +3,38 @@ const converter = require('@iota/converter')
 import { convertMessage } from './convertMessage'
 
 const iota = composeAPI({
-    'provider': 'https://nodes.thetangle.org:443'
+  provider: 'https://nodes.thetangle.org:443'
 })
 
-const fetchFromIota = (bundle) => {
-    let searchVarsBundle = {'bundles': [bundle]}
+const fetchFromIota = bundle => {
+  let searchVarsBundle = { bundles: [bundle] }
 
-    return iota.findTransactions(searchVarsBundle)
-            .then(hashes => {
-                return iota.getBundle(hashes[0])
-                        .then(bundle => {
-                            const message = bundle[0].signatureMessageFragment.replace(/\d+$/, "")
-                            const descrypted = converter.trytesToAscii(message)
-                            return {
-                                error: false,
-                                message: convertMessage(descrypted)
-                            }
-                        })
-                        .catch(err => {
-                            return {
-                                error: true,
-                                message: err.message
-                            }
-                        })
-            })
-            .catch(err => {
-                return {
-                    error: true,
-                    message: err.message
-                }
-            })
+  return iota
+    .findTransactions(searchVarsBundle)
+    .then(hashes => {
+      return iota
+        .getBundle(hashes[0])
+        .then(bundle => {
+          const message = bundle[0].signatureMessageFragment.replace(/\d+$/, '')
+          const descrypted = converter.trytesToAscii(message)
+          return {
+            error: false,
+            message: convertMessage(descrypted)
+          }
+        })
+        .catch(err => {
+          return {
+            error: true,
+            message: err.message
+          }
+        })
+    })
+    .catch(err => {
+      return {
+        error: true,
+        message: err.message
+      }
+    })
 }
 
 export default fetchFromIota
